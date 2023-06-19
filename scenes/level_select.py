644cb5187aa1b83a0fd50ddbccf1e3dbe1af82a0
Jessica Halvorsen <92817583+jessicahalvorsen@users.noreply.github.com>
2023-06-19 00:05:16 -0400

    Update level_select.py
    Updated to assign the correct coin images based on the save file info for each level

--- scenes/level_select.py
+++ scenes/level_select.py
@@ -8,15 +8,6 @@ import math
     author: Jessica Halvorsen <j.halvorsen@ufl.edu>
 
 """
-
-"""
-def draw_coins(screen, level_coins, gold, silver):
-    for i in range(0, 2):
-        curr_num = int(level_coins[i])
-        if curr_num >= 1:
-    # print first gold coin for the current level
-"""
-
 def run_level_select_screen(screen):
     # load background image and scale it to fit in the screen window
     background = pygame.image.load("../images/level_select_screen/level-no-circle.png")
@@ -41,17 +32,32 @@ def run_level_select_screen(screen):
     credits_img = pygame.transform.scale(credits_img, (w / 6, h / 18))
     credits_btn = button.Button(credits_img)
 
-    # TODO: decide how we want to do the coins being gold/silver
     # load level information about coins from the save file
     with open("../game_data", 'r') as file:
         level_coins = [line.rstrip() for line in file]
 
-    # load gold and silver coin images
-    gold_3_img = pygame.image.load("../images/level_select_screen/3-gold-coins.png")
-    gold_3_img = pygame.transform.scale(gold_3_img, (w / 5, h / 7))
+    # decide which coin images correspond to each level
+    level_coin_imgs = []
+    for i in range(0, 3):
+        temp = int(level_coins[i])
+        if temp == 3:
+            level_coin_imgs.append("3-gold-coins.png")
+        elif temp == 2:
+            level_coin_imgs.append("2-gold-coins.png")
+        elif temp == 1:
+            level_coin_imgs.append("1-gold-coins.png")
+        elif temp == 0:
+            level_coin_imgs.append("0-gold-coins.png")
 
-    gold_0_img = pygame.image.load("../images/level_select_screen/3-silver-coins.png")
-    gold_0_img = pygame.transform.scale(gold_0_img, (w / 5, h / 7))
+    # load level coin images
+    lvl_1_coin_img = pygame.image.load(f"../images/level_select_screen/{level_coin_imgs[0]}")
+    lvl_1_coin_img = pygame.transform.scale(lvl_1_coin_img, (w / 5, h / 7))
+
+    lvl_2_coin_img = pygame.image.load(f"../images/level_select_screen/{level_coin_imgs[1]}")
+    lvl_2_coin_img = pygame.transform.scale(lvl_2_coin_img, (w / 5, h / 7))
+
+    lvl_3_coin_img = pygame.image.load(f"../images/level_select_screen/{level_coin_imgs[2]}")
+    lvl_3_coin_img = pygame.transform.scale(lvl_3_coin_img, (w / 5, h / 7))
 
     # driver loop setup
     running = True
@@ -77,32 +83,42 @@ def run_level_select_screen(screen):
                     return "credits"
             # if screen is resized rescale all buttons and images
             elif event.type == pygame.VIDEORESIZE:
+                # display screen background and get new screen dimensions
                 screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
                 background = pygame.transform.scale(background, event.size)
                 w, h = pygame.display.get_surface().get_size()
+
+                # display buttons proportional to new dimensions
                 button_1_img = pygame.transform.scale(button_1_img, (h / 4, h / 4))
                 button_1.image = button_1_img
                 button_2_img = pygame.transform.scale(button_2_img, (h / 4, h / 4))
                 button_2.image = button_2_img
                 button_3_img = pygame.transform.scale(button_3_img, (h / 4, h / 4))
                 button_3.image = button_3_img
+
+                # display the credits button
                 credits_img = pygame.transform.scale(credits_img, (w / 6, h / 18))
                 credits_btn.image = credits_img
-                # draw correct color coin in the correct location
-                gold_3_img = pygame.transform.scale(gold_3_img, (w / 5, h / 7))
-                gold_0_img = pygame.transform.scale(gold_0_img, (w / 5, h / 7))
-                # coin_gold_img = pygame.transform.scale(coin_gold_img, (h / 12, h / 12))
-                # coin_silver_img = pygame.transform.scale(coin_silver_img, (h / 12, h / 12))
+
+                # draw correct color coin with the correct size
+                lvl_1_coin_img = pygame.transform.scale(lvl_1_coin_img, (w / 5, h / 7))
+                lvl_2_coin_img = pygame.transform.scale(lvl_2_coin_img, (w / 5, h / 7))
+                lvl_3_coin_img = pygame.transform.scale(lvl_3_coin_img, (w / 5, h / 7))
                 break
 
         # draw the background and buttons with scaled position
         screen.blit(background, (0, 0))
+
         button_1.draw(screen, (w * (1.5 / 7), h * (4 / 5)))
         button_2.draw(screen, (w * (3.5 / 7), h * (7 / 10)))
         button_3.draw(screen, (w * (5.5 / 7), h * (7 / 9)))
-        credits_btn.draw(screen, (w * 0.90, h * 0.95))
-        screen.blit(gold_3_img, (w * (0.8 / 7), h * (5 / 6)))
-        screen.blit(gold_0_img, (w * (2.8 / 7), h * (8.15 / 11)))
+
+        credits_btn.draw(screen, (w * 0.90, h * 0.97))
+
+        screen.blit(lvl_1_coin_img, (w * (0.8 / 7), h * (5 / 6)))
+        screen.blit(lvl_2_coin_img, (w * (2.8 / 7), h * (8.15 / 11)))
+        screen.blit(lvl_3_coin_img, (w * (4.8 / 7), h * (8.15 / 10)))
+
         pygame.display.flip()
 
 
@@ -112,4 +128,3 @@ def run_level_select_screen(screen):
 # tori gate: https://pixabay.com/photos/miyajima-gate-tori-547290/
 # mountain lake: https://pixabay.com/photos/mount-fuji-japan-mountains-landmark-395047/
 # buddha: https://pixabay.com/photos/buddha-buddha-statue-spiritual-4014365/
-
