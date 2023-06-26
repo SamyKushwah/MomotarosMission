f6554c6eb4d028f0b51bef6d625a37559b67c864
Jessica Halvorsen <j.halvorsen@ufl.edu>
2023-06-26 11:23:55 +0900

    Fixed lag
    - changed the platforms to only load the images once instead of for every platform to get rid of the lag
    - updated the button img

--- scenes/levels/level_1.py
+++ scenes/levels/level_1.py
@@ -3,27 +3,31 @@ from game_templates import level
 
 def create_level(my_toolbox):
     level1 = level.Level(my_toolbox, 1, 5100, 5000, background="mountains")
+    level1.load_water_img()
+    level1.load_stone_imgs()
+
     # add four walls
-    level1.add_platform(position=(0, 0), dimensions=(71, 1080), facing_direction="right")  # left wall
-    level1.add_platform((5100 - 70, 0), (70, 1080), facing_direction="left")  # right wall
-    level1.add_platform((0, 1080 - 70), (1700, 70))  # bottom wall part 1
-    level1.add_platform((0, 0), (5100, 140), facing_direction="down")               # top wall
+    level1.add_platform(position=(0, 0), dimensions=(71, 1080), facing_direction="right")       # left wall
+    level1.add_platform((5100 - 70, 0), (70, 1080), facing_direction="left")                    # right wall
+    level1.add_platform((0, 1080 - 70), (1700, 70))                                             # bottom wall part 1
+    level1.add_platform((0, 0), (5100, 140), facing_direction="down")                           # top wall
 
     # fix the corners
+    level1.add_platform((-10, 70), (70, 70), facing_direction=None)
     level1.add_platform((5040, 70), (70, 70), facing_direction=None)
     level1.add_platform((-10, 1080 - 70), (70, 70), facing_direction=None)
     level1.add_platform((5040, 1080 - 70), (70, 70), facing_direction=None)
 
     # add platforms
-    level1.add_platform((400, 800), (300, 50), facing_direction="up") #platform 1 for coin 1
-    level1.add_platform((900, 690), (300, 50), facing_direction="up") #platform 2 for coin 1
-    level1.add_obstacle(1080, 600, "coin")   # coin 1
+    level1.add_platform((400, 800), (300, 50), facing_direction="up")               # platform 1 for coin 1
+    level1.add_platform((900, 690), (300, 50), facing_direction="up")               # platform 2 for coin 1
+    level1.add_obstacle(1080, 600, "coin")                                          # coin 1
 
     # gate leading to after the first coin
     level1.add_obstacle(1265, 1000, 'button', fence_initial=(1450, 780), fence_final=(1450, 540), fence_dimensions=(200, 500))
 
     # block to cover off the gate top
-    level1.add_platform((1350, 140), (215, 500), facing_direction="all")
+    level1.add_platform((1350, 130), (215, 500), facing_direction="all")
 
 
     # platforming water section
