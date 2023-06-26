ed7014fc3ee85a81f7f0076f122e1777a8b7b730
Jessica Halvorsen <j.halvorsen@ufl.edu>
2023-06-26 13:26:16 +0900

    updated win/lose screen
    new push changes:
    - updated win screen that displays the number of coins won
    - added code in game_manager.py to update the save file data after a completed mission
    - added a new lose_screen background and shifted button position

--- scenes/pause_screen_scene.py
+++ scenes/pause_screen_scene.py
@@ -13,34 +13,22 @@ def run(my_toolbox: toolbox.Toolbox, current_level):
 
     # load home button image
     home_img = pygame.Surface((400, 110), pygame.SRCALPHA)
-    home_img.fill((255, 255, 255, 128))
-    #home_img = pygame.image.load("images/pause_screen/home_btn.png")
-    #home_img = pygame.transform.scale(home_img, (w * (1 / 4), h * (1 / 13)))
-    button_home = button.Button(home_img, lwd=2, text="Home")
+    home_img.fill((255, 255, 255, 0))
+    button_home = button.Button(home_img, text="Home")
 
-    """home_highlight = pygame.image.load("images/pause_screen/homehighlight.png")
-    home_highlight = pygame.transform.scale(home_highlight, (w * (1 / 4), h * (1 / 13)))
-    home_highlight_bt = button.Button(home_highlight)"""
-
-    #load restart button image
-    #restart_img = pygame.image.load("images/pause_screen/restart_btn.png")
-    #restart_img = pygame.transform.scale(restart_img, (w * (1 / 3), h * (1 / 12)))
+    # load restart button image
     restart_img = pygame.Surface((610, 115), pygame.SRCALPHA)
-    restart_img.fill((255, 255, 255, 128))
-    button_restart = button.Button(restart_img, lwd=2, text="Restart")
+    restart_img.fill((255, 255, 255, 0))
+    button_restart = button.Button(restart_img, text="Restart")
 
-    #load resume button image
-    #resume_img = pygame.image.load("images/pause_screen/resume_btn.png")
-    #resume_img = pygame.transform.scale(resume_img, (w * (1 / 3), h * (1 / 12)))
+    # load resume button image
     resume_img = pygame.Surface((575, 115), pygame.SRCALPHA)
-    resume_img.fill((255, 255, 255, 128))
-    button_resume = button.Button(resume_img, lwd=2, text="Resume")
+    resume_img.fill((255, 255, 255, 0))
+    button_resume = button.Button(resume_img, text="Resume")
 
+    # draw the background
     scene_screen = pygame.surface.Surface((w, h))
     scene_screen.blit(background, (0, 0))
-    #scene_screen.set_alpha(170)
-
-    #highlight = False
 
     # driver loop setup
     running = True
@@ -55,8 +43,6 @@ def run(my_toolbox: toolbox.Toolbox, current_level):
                 if button_home.is_clicked(my_toolbox.adjusted_mouse_pos(event.pos)):
                     return "level_selector"
                 elif button_restart.is_clicked(my_toolbox.adjusted_mouse_pos(event.pos)):
-                    #print('returning current level')
-                    #print(current_level)
                     return current_level
                 elif button_resume.is_clicked(my_toolbox.adjusted_mouse_pos(event.pos)):
                     return "resume"
@@ -64,13 +50,13 @@ def run(my_toolbox: toolbox.Toolbox, current_level):
                 if event.key == pygame.K_ESCAPE:
                     return "resume"
 
-
-        # draw the background and buttons with scaled position
-
+        # draw the buttons with scaled position
         button_resume.draw(scene_screen, (w / 2, h * (6 / 13)), True)
         button_restart.draw(scene_screen, (w / 2, h * (8 / 13)), True)
         button_home.draw(scene_screen, (w / 2, h * (10 / 13)), True)
 
-        #scene_screen.set_alpha(170)
+        # scene_screen.set_alpha(170)
         my_toolbox.draw_to_screen(scene_screen)
         pygame.display.flip()
+
+        my_toolbox.clock.tick(60)
--- scenes/win_screen_scene.py
+++ scenes/win_screen_scene.py
@@ -4,27 +4,39 @@ from ui_templates import button
 from drivers import toolbox
 
 
-def run(my_toolbox: toolbox.Toolbox, current_level):
+def run(my_toolbox: toolbox.Toolbox, current_level, coins):
     w, h = 1920, 1080
 
     # load background image and scale it to fit in the screen window
-    background = pygame.image.load("images/win_screen/win_screen.png")
+    if coins == 0:
+        background = pygame.image.load("images/win_screen/win_screen_0.png")
+    elif coins == 1:
+        background = pygame.image.load("images/win_screen/win_screen_1.png")
+    elif coins == 2:
+        background = pygame.image.load("images/win_screen/win_screen_2.png")
+    else:
+        background = pygame.image.load("images/win_screen/win_screen_3.png")
+
     background = pygame.transform.scale(background, (w, h))
 
     # load home button image
-    home_img = pygame.image.load("images/win_screen/home_btn.png")
-    home_img = pygame.transform.scale(home_img, (w * (1 / 5), h * (1 / 14)))
-    button_home = button.Button(home_img)
+    home_img = pygame.Surface((400, 110), pygame.SRCALPHA)
+    home_img.fill((255, 255, 255, 0))
+    button_home = button.Button(home_img, text="Home")
 
     # load restart button image
-    restart_img = pygame.image.load("images/win_screen/restart_btn.png")
-    restart_img = pygame.transform.scale(restart_img, (w * (1 / 3), h * (1 / 13)))
-    button_restart = button.Button(restart_img)
+    restart_img = pygame.Surface((610, 115), pygame.SRCALPHA)
+    restart_img.fill((255, 255, 255, 0))
+    button_restart = button.Button(restart_img, text="Restart")
 
     # load next button image
-    next_img = pygame.image.load("images/win_screen/next_btn.png")
-    next_img = pygame.transform.scale(next_img, (w * (1 / 5), h * (1 / 14)))
-    button_next = button.Button(next_img)
+    next_img = pygame.Surface((400, 105), pygame.SRCALPHA)
+    next_img.fill((255, 255, 255, 0))
+    button_next = button.Button(next_img, text="Next")
+
+    # draw the background and buttons with scaled position
+    scene_screen = pygame.surface.Surface((w, h))
+    scene_screen.blit(background, (0, 0))
 
     # driver loop setup
     running = True
@@ -40,16 +52,10 @@ def run(my_toolbox: toolbox.Toolbox, current_level):
                     return "level_selector"
                 elif button_restart.is_clicked(my_toolbox.adjusted_mouse_pos(event.pos)):
                     return current_level
-                # elif button_next.is_clicked(my_toolbox.adjusted_mouse_pos(event.pos)):
-                # return "level_2" if pressed if we make a level 2
 
-        # draw the background and buttons with scaled position
-        scene_screen = pygame.surface.Surface((w, h))
-        scene_screen.blit(background, (0, 0))
-
-        button_home.draw(scene_screen, (w * (1 / 7), h * (12 / 13)))
-        button_restart.draw(scene_screen, (w * (1 / 2), h * (12 / 13)))
-        button_next.draw(scene_screen, (w * (6 / 7), h * (12 / 13)))
+        button_home.draw(scene_screen, (w * (1 / 7), h * (12 / 13)), True)
+        button_restart.draw(scene_screen, (w * (1 / 2), h * (12 / 13)), True)
+        button_next.draw(scene_screen, (w * (6 / 7), h * (12 / 13)), True)
 
         my_toolbox.draw_to_screen(scene_screen)
 
