bfbce35bccd79e10adaf3a9f4f73be9d7d1e0a0e
SamyKushwah <73620782+SamyKushwah@users.noreply.github.com>
2023-06-26 12:43:11 +0900

    Updated and cleaned button.py file for highlighting

--- ui_templates/button.py
+++ ui_templates/button.py
@@ -7,67 +7,52 @@ pygame.font.init()
 
 
 class Button(pygame.sprite.Sprite):
-    button_font = pygame.font.SysFont('Comic Sans MS', 30)
-
     my_toolbox = toolbox.Toolbox()
+
     # loading the font
     font_path = "snapitc.ttf"
-    font_size = 135
-    snapitc_font = pygame.font.Font(font_path, font_size)
+    #font_size = 135
 
-    def __init__(self, image, font=font_path, lwd=None, text=None):
+    def __init__(self, image, font=font_path, lwd=None, text=None, font_size=135):
         pygame.sprite.Sprite.__init__(self)
         self.font = font
         self.lwd = lwd
         self.text = text
         self.image = image
         self.rect = self.image.get_rect()
-
+        self.font_size = font_size
         self.hover = False
         self.mous_pos = None
 
-    def draw(self, surface, location, highlight = False):
+    def draw(self, surface, location, highlight=False):
+        # drawing button
         self.rect.center = location
         surface.blit(self.image, self.rect)
 
-        text_surface = None
+        # text_surface = None
         if self.font is not None and self.text is not None:
+            # creating red text
             my_font = pygame.font.Font("drivers/" + self.font, self.font_size)
             text_surface = my_font.render(self.text, False, (195, 52, 39))  # red - 195, 52, 39
             text_rect = text_surface.get_rect()  # Set text_rect center
             text_rect.center = location
             surface.blit(text_surface, text_rect)
             if highlight:
+                # seeing if cursor is hovering on button
                 mouse_temp = pygame.mouse.get_pos()
                 mouse_pos = self.my_toolbox.adjusted_mouse_pos(mouse_temp)
-                # mouse_pos = pygame.mouse.get_pos()
-                # print(self.rect)
-                # print(text_rect)
                 self.hover = self.rect.collidepoint(mouse_pos)
-                # print(mouse_pos)
                 if self.hover:
-                    # print(location)
-                    # Create a shadow effect for the text
-                    shadow_offset = 2  # Offset for the shadow
-                    shadow_color = (0, 0, 0)  # Shadow color (black in this example)
+                    # create a different color for cursor text
+                    shadow_offset = 0
+                    shadow_color = (0, 0, 0)  # Text color (yellow)
                     shadow_pos = (location[0] + shadow_offset, location[1] + shadow_offset)
+                    # creating yellow text
                     my_font = pygame.font.Font("drivers/" + self.font_path, self.font_size)
                     text_surface = my_font.render(self.text, True, shadow_color)
                     text_rect = text_surface.get_rect()  # Set text_rect center
                     text_rect.center = shadow_pos
                     surface.blit(text_surface, text_rect)
-                # print(self.hover)
-                # text_surface = my_font.render(self.text, False, (0, 0, 0))
-                # text_yellow_rect = text_surface.get_rect()
-                # text_rect_prnt = text_yellow_rect if self.hover else text_rect
-                '''if self.hover and mouse_pos == self.mouse_pos:
-                    self.count += 1
-                    if self.count > 10:
-                        self.kill()
-                else:
-                    self.count = 0'''
-                # self.mouse_pos = mouse_pos
-                # surface.blit(text_surface, text_rect_prnt)
 
     def is_clicked(self, mouse_pos):
         return self.rect.collidepoint(mouse_pos)
