e7dfff5f715bf16ee3cc9b2225b93ce94b9d755c
Shawn Rhoads <76660850+shawn-rhoads@users.noreply.github.com>
2023-07-13 15:00:12 +0900

    Added second player (bird only), tweaked interactibles, collision, demon movement to work with second player
    ~6 hours of work

---
+++ drivers/game_manager.py
@@ -0,0 +1,229 @@
+from scenes.levels import level_1A, level_1
+import pygame
+from game_templates import momotaro_player, pet_player
+from scenes import pause_screen_scene, win_screen_scene, lose_screen_scene
+from ui_templates import button
+import sys
+
+'''
+Purpose: The GameManager object contains level and player information and regularly updates and polls player 
+            interactions with the environment provided by the level.
+'''
+# "Persistent State" Game Manager:
+class GameManager:
+    def __init__(self, my_toolbox, level):
+        self.my_toolbox = my_toolbox
+        self.level_complete = False
+        self.momotaro = momotaro_player.Momotaro([300, 300])
+        self.pet = pet_player.Pet([400, 300])
+        self.coins_collected = 0
+        self.level_name = level
+        match level:
+            case "level_1A":
+                self.level = level_1A.create_level(my_toolbox)
+            case "level_1":
+                self.level = level_1.create_level(my_toolbox)
+
+        self.image = pygame.surface.Surface((self.level.width, self.level.height))
+
+        # Creating pause button
+        pause_img = pygame.image.load("images/game_ui/pause_btn.png").convert_alpha()
+        pause_img = pygame.transform.scale(pause_img, (90, 70))
+        self.pause_btn = button.Button(pause_img)
+
+        # Loading background image
+        self.mountain_background = pygame.transform.scale(
+        #    pygame.image.load("images/backgrounds/mountains/parallax-mountain-bg-reduced.png").convert_alpha(), (1920, 1080))
+            pygame.image.load("images/backgrounds/level_1_bkgnd.png").convert_alpha(), (1920, 915))
+        #self.far_mountains = pygame.image.load("images/backgrounds/mountains/parallax-mountain-mountains-reduced.png").convert_alpha()
+
+        self.controls = pygame.image.load("images/game_ui/controls.png").convert_alpha()
+
+    '''
+    Purpose: While the GameManager object is running, the main gameplay loop for the corresponding level occurs
+    '''
+    def run(self):
+        # run event handling for the level until lvl_complete == True or broken out of
+        while not self.level_complete:
+            # Poll events/user inputs
+            events = pygame.event.get()
+            for event in events:
+                # Exiting the Game
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:  # if clicking, can click on pause button
+                    if self.pause_btn.is_clicked(
+                            self.my_toolbox.adjusted_mouse_pos(event.pos)):  # if clicked pause button
+                        return_st = pause_screen_scene.run(self.my_toolbox, self.level_name)
+                        if return_st == "level_selector" or return_st == self.level_name:  # break out of running level
+                            #print('restarting')
+                            return return_st
+
+                # if key pressing, can either be pausing or ending the game
+                elif event.type == pygame.KEYDOWN:
+                    # pause button pressed
+                    if event.key == pygame.K_ESCAPE:
+                        return_st = pause_screen_scene.run(self.my_toolbox, self.level_name)
+
+                        # Poll pause scene next scene
+                        if return_st == "level_selector" or return_st == self.level_name:  # break out of running level
+                            #print('restarting')
+                            return return_st
+                    # up button pressed (W) at the tori gate, ending the level
+                    if self.level.interactible_list["torigate"][0].is_pushed() and event.key == pygame.K_w:
+                        win_return = win_screen_scene.run(self.my_toolbox, self.level_name, self.coins_collected)
+                        self.update_save_file(self.level_name, self.coins_collected)
+                        # Poll the win game scene next scene
+                        if win_return == "level_selector" or win_return == self.level_name or win_return ==  "quit":
+                            return win_return
+
+            # Checking for if the game is over/failed (Momo dead or out of bounds)
+            if self.momotaro.health <= 0 or self.momotaro.position[1] > 4000 or self.pet.health <= 0:
+                lose_rt = lose_screen_scene.run(self.my_toolbox, self.level_name)
+
+                # Poll next scene from lose screen
+                if lose_rt == "level_selector" or lose_rt == self.level_name or lose_rt == "quit":
+                    return lose_rt
+
+            # If momotaro is pushed below a block, he dies
+            
+            if self.momotaro.standing_on:
+                if self.momotaro.position[
+                    1] + self.momotaro.get_rect().height // 2 > self.momotaro.standing_on.get_rect().top:
+                    lose_rt = lose_screen_scene.run(self.my_toolbox, self.level_name)
+                    if lose_rt == "level_selector" or lose_rt == self.level_name or lose_rt == "quit":
+                        return lose_rt
+
+            self.tick_physics()
+            self.draw()
+
+            self.my_toolbox.clock.tick(60)
+
+
+    '''
+    Purpose: Main physics checker and updater for gameplay cycle. Updates character and enemy collision, movement, and
+                interaction with obstacles and with each other. Specific details on what's being updated belong within 
+                the character methods.
+    '''
+    def tick_physics(self):
+        for moving_platform in self.level.moving_platform_list:
+            moving_platform.movement()
+
+        # Reset interactibles
+        for interactible in self.level.interactible_list["button"]:
+            interactible.set_pushed(False)
+
+        self.momotaro.update_movement()
+        self.momotaro.check_collisions(self.level.collidable_list)
+        self.momotaro.check_collision_interactible(self.level.interactible_list, self)
+        self.momotaro.check_damage(self.level.demon_list)
+        self.momotaro.check_attacking(self.level.demon_list)
+
+        self.pet.update_movement()
+        self.pet.check_collisions(self.level.collidable_list)
+        self.pet.check_collision_interactible(self.level.interactible_list, self)
+        self.pet.check_damage(self.level.demon_list)
+
+        #print(self.momotaro.position)
+        for demon in self.level.demon_list:
+            demon.update_movement(self.momotaro, self.pet)
+            demon.check_collisions(self.level.collidable_list)
+
+
+    '''
+    Purpose: Calls the draw function of each object and draws backgrounds
+    '''
+    def draw(self):
+        view_surface = pygame.surface.Surface((1920, 1080))
+
+        # Draw Background
+        self.image.fill((70, 70, 180))
+        match self.level.background:
+            case "cave":
+                self.image.fill((20, 20, 30))
+            case "mountains":
+                if self.momotaro.get_rect().centerx <= 960:
+                    positional = 0 - (960 / 200)
+                elif self.momotaro.get_rect().centerx >= self.level.width - 960:
+                    positional = (self.level.width - 960) - ((self.level.width - 960) / 200) - 960
+                else:
+                    positional = self.momotaro.get_rect().centerx - (self.momotaro.get_rect().centerx / 200) - 960
+
+                # Main Background
+                self.image.blit(self.mountain_background, (positional, 100))
+                self.image.blit(self.mountain_background, (1920 + positional, 100))
+
+                self.image.blit(self.controls, (170, 200))
+
+                # Far Mountains
+                #self.image.blit(self.far_mountains, (-544 + positional, 850))
+                #self.image.blit(self.far_mountains, (positional, 850))
+                #self.image.blit(self.far_mountains, (544 + positional, 850))
+
+        # Draw platforms
+        for platform in self.level.platform_list:
+            platform.draw_platform(self.image)
+        for platform in self.level.moving_platform_list:
+            platform.draw_platform(self.image)
+        for text in self.level.tutorial_text_list:
+            text.draw(self.image, self.momotaro.position[0])
+
+        # Draw demons
+        for demon in self.level.demon_list:
+            if demon.health > 0:
+                demon.draw(self.image)
+            else:
+                self.level.demon_list.remove(demon)
+
+        # Draw obstacles
+        for interactible_key in self.level.interactible_list.keys():
+            match interactible_key:
+                case "button":
+                    for obstacle in self.level.interactible_list[interactible_key]:
+                        obstacle.draw(self.image)
+                case "torigate":
+                    for obstacle in self.level.interactible_list[interactible_key]:
+                        obstacle.draw(self.image)
+                case "coin":
+                    #self.level.coins_collected = 0
+                    for coin in self.level.interactible_list[interactible_key]:
+                        if not coin.collected:
+                            coin.draw(self.image)
+
+        # Draw players
+        self.momotaro.draw(self.image)
+        self.pet.draw(self.image)
+
+        if self.momotaro.get_rect().centerx <= 960:
+            view_surface.blit(self.image, (0, 0))
+        elif self.momotaro.get_rect().centerx >= self.level.width - 960:
+            view_surface.blit(self.image, (-(self.level.width - 1920), 0))
+        else:
+            view_surface.blit(self.image, ((-self.momotaro.get_rect().centerx) + (1920 / 2), 0))
+
+        # Draw Header
+        self.level.header.draw_header(view_surface, self.momotaro.health, self.pet.health, self.coins_collected)
+
+        #self.pause_btn.draw(view_surface, (80, 65))
+        self.my_toolbox.draw_to_screen(view_surface)
+        pygame.display.update()
+
+    """
+    Purpose: Saves the number of coins collected upon level clear to update on the select screen
+    """
+    def update_save_file(self, level_name, coins_collected):
+        # get current info from the save file
+        with open("save_data/game_data", 'r') as file:
+            level_coins = [line.rstrip() for line in file]
+
+        # depending on which level you are currently on, update the information
+        if level_name == "level_1":
+            level_coins[0] = coins_collected
+        elif level_name == "level_1A":
+            level_coins[1] = coins_collected
+        else:
+            level_coins[2] = coins_collected
+
+        with open("save_data/game_data", 'w') as file:
+            [file.write(str(coin) + "\n") for coin in level_coins]
---
+++ game_templates/level.py
@@ -0,0 +1,339 @@
+import pygame
+import math
+
+from game_templates import demon, obstacles
+
+from ui_templates import tutorial
+
+class Level:
+    def __init__(self, my_toolbox, level_num, level_width, level_height, background = "cave"):
+        self.width = level_width
+        self.height = level_height
+        self.level_num = level_num
+        self.collidable_list = []
+        self.platform_list = []
+        self.moving_platform_list = []
+        self.tutorial_text_list = []
+        self.interactible_list = {}
+        self.demon_list = []
+        self.background = background
+        self.header = Header()
+        self.stone_imgs = []
+        self.water_img = None
+
+    def add_platform(self, position, dimensions, platform_type = "stone", facing_direction="all", corners=False):
+        temp_platform = Platform(position, dimensions, self.stone_imgs, self.water_img, platform_type, facing_direction, corners)
+        self.platform_list.append(temp_platform)
+        self.collidable_list.append(temp_platform)
+
+    def add_moving_platform(self, position, dimensions, max_speed, target, platform_type="stone",
+                            facing_direction="all",
+                            corners=False):
+        temp_platform = MovingPlatform(position, dimensions, max_speed, target, self.stone_imgs, self.water_img, platform_type, facing_direction,
+                                       corners)
+        #print("Adding platform", temp_platform.get_rect())
+        self.moving_platform_list.append(temp_platform)
+        self.collidable_list.append(temp_platform)
+
+    def add_demon(self, spawn_position, detection_range):
+        temp_demon = demon.Demon(spawn_position, detection_range)
+        self.demon_list.append(temp_demon)
+
+    def add_obstacle(self, x, y, type, fence_initial = None, fence_final = None, fence_dimensions = None):
+        match type:
+            case "button":
+                temp_obstacle = obstacles.ButtonObstacle((x,y), fence_initial, fence_final, x, y, fence_dimensions)
+                try:
+                    self.interactible_list["button"] += [temp_obstacle]
+                except KeyError:
+                    self.interactible_list["button"] = [temp_obstacle]
+
+                self.collidable_list.append(temp_obstacle)
+
+                temp_obstacle = temp_obstacle.fence
+                try:
+                    self.interactible_list["fence"] += [temp_obstacle]
+                except KeyError:
+                    self.interactible_list["fence"] = [temp_obstacle]
+
+                self.collidable_list.append(temp_obstacle)
+
+            case "torigate":
+                temp_obs = obstacles.ToriObstacle(x, y)
+                try:
+                    self.interactible_list["torigate"] += [temp_obs]
+                except KeyError:
+                    self.interactible_list["torigate"] = [temp_obs]
+
+            case "coin":
+                temp_obs = obstacles.CoinObstacle(x, y)
+                try:
+                    self.interactible_list["coin"] += [temp_obs]
+                except KeyError:
+                    self.interactible_list["coin"] = [temp_obs]
+
+    def add_tutorial_text(self,x,y,x_min,x_max,dimensions,text):
+        temp_text = tutorial.TutorialText((x,y), x_min, x_max, dimensions=dimensions, text= text)
+        self.tutorial_text_list.append(temp_text)
+
+    def load_stone_imgs(self):
+        self.stone_imgs.append(pygame.image.load("images/tiles/stone/Stone(BL).png").convert_alpha())
+        self.stone_imgs.append(pygame.image.load("images/tiles/stone/Stone(BM).png").convert_alpha())
+        self.stone_imgs.append(pygame.image.load("images/tiles/stone/Stone(BR).png").convert_alpha())
+        self.stone_imgs.append(pygame.image.load("images/tiles/stone/Stone(ML).png").convert_alpha())
+        self.stone_imgs.append(pygame.image.load("images/tiles/stone/Stone(MM).png").convert_alpha())
+        self.stone_imgs.append(pygame.image.load("images/tiles/stone/Stone(MR).png").convert_alpha())
+        self.stone_imgs.append(pygame.image.load("images/tiles/stone/Stone(TL).png").convert_alpha())
+        self.stone_imgs.append(pygame.image.load("images/tiles/stone/Stone(TM).png").convert_alpha())
+        self.stone_imgs.append(pygame.image.load("images/tiles/stone/Stone(TR).png").convert_alpha())
+
+    def load_water_img(self):
+        self.water_img = pygame.image.load("images/tiles/watertile.png").convert_alpha()
+
+
+class Platform:
+    def __init__(self, position, dimensions, stone_imgs, water_img, platform_type, facing_direction, corners=False):
+        self.width = dimensions[0]
+        self.height = dimensions[1]
+        self.x = position[0]
+        self.y = position[1]
+        self.image = pygame.surface.Surface(dimensions)
+        self.velocity = [0, 0]
+        self.type = platform_type
+        match platform_type:
+            case "stone":
+                BL = stone_imgs[4]
+                BM = stone_imgs[4]
+                BR = stone_imgs[4]
+                ML = stone_imgs[4]
+                MM = stone_imgs[4]
+                MR = stone_imgs[4]
+                TL = stone_imgs[4]
+                TM = stone_imgs[4]
+                TR = stone_imgs[4]
+                match facing_direction:
+                    case "all":
+                        BL = stone_imgs[0]
+                        BM = stone_imgs[1]
+                        BR = stone_imgs[2]
+                        ML = stone_imgs[3]
+                        MM = stone_imgs[4]
+                        MR = stone_imgs[5]
+                        TL = stone_imgs[6]
+                        TM = stone_imgs[7]
+                        TR = stone_imgs[8]
+                    case "up":
+                        TM = stone_imgs[7]
+                        if corners:
+                            TL = stone_imgs[6]
+                            TR = stone_imgs[8]
+                        else:
+                            TL = TM
+                            TR = TM
+                    case "down":
+                        BM = stone_imgs[1]
+                        if corners:
+                            BL = stone_imgs[0]
+                            BR = stone_imgs[2]
+                        else:
+                            BL = BM
+                            BR = BM
+                    case "left":
+                        ML = stone_imgs[3]
+                        if corners:
+                            BL = stone_imgs[0]
+                            TL = stone_imgs[6]
+                        else:
+                            BL = ML
+                            TL = ML
+                    case "right":
+                        MR = stone_imgs[5]
+                        if corners:
+                            BR = stone_imgs[2]
+                            TR = stone_imgs[4]
+                        else:
+                            BR = MR
+                            TR = MR
+
+            case "water":
+                BL = water_img
+                BM = water_img
+                BR = water_img
+                ML = water_img
+                MM = water_img
+                MR = water_img
+                TL = water_img
+                TM = water_img
+                TR = water_img
+
+        BL = pygame.transform.scale(BL, (70, 70))
+        BM = pygame.transform.scale(BM, (70, 70))
+        BR = pygame.transform.scale(BR, (70, 70))
+        ML = pygame.transform.scale(ML, (70, 70))
+        MM = pygame.transform.scale(MM, (70, 70))
+        MR = pygame.transform.scale(MR, (70, 70))
+        TL = pygame.transform.scale(TL, (70, 70))
+        TM = pygame.transform.scale(TM, (70, 70))
+        TR = pygame.transform.scale(TR, (70, 70))
+
+        tile_width = int(MM.get_size()[0])
+        tile_height = int(MM.get_size()[1])
+        tiles_wide = int(math.ceil(self.width / (tile_width - 0)))
+        tiles_high = int(math.ceil(self.height / (tile_height - 0)))
+
+        for column in range(0, tiles_wide):
+            if column == 0:
+                self.image.blit(TL, (tile_width * column, 0))
+            elif column == tiles_wide - 1:
+                self.image.blit(TR, (self.width - tile_width, 0))
+            else:
+                self.image.blit(TM, (tile_width * column, 0))
+
+        for row in range(1, tiles_high - 1):
+            for column in range(0, tiles_wide):
+                if column == 0:
+                    self.image.blit(ML, (tile_width * column, tile_height * row))
+                elif column == tiles_wide - 1:
+                    self.image.blit(MR, (self.width - tile_width, tile_height * row))
+                else:
+                    self.image.blit(MM, (tile_width * column, tile_height * row))
+
+        if tiles_high > 1:
+            for column in range(0, tiles_wide):
+                if column == 0:
+                    self.image.blit(BL, (tile_width * column, self.height - tile_height))
+                elif column == tiles_wide - 1:
+                    self.image.blit(BR, (self.width - tile_width, self.height - tile_height))
+                else:
+                    self.image.blit(BM, (tile_width * column, self.height - tile_height))
+
+    def get_rect(self):
+        return pygame.rect.Rect((self.x, self.y), (self.width, self.height))
+
+    def draw_platform(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+
+class MovingPlatform(Platform):
+    def __init__(self, position, dimensions, max_speed, target, stone_imgs, water_img, platform_type, facing_direction="all", corners=False):
+        super().__init__(position, dimensions, stone_imgs, water_img, platform_type, facing_direction, corners)
+        self.__int_x = position[0]
+        self.__int_y = position[1]
+        self.__moving_right = True
+        self.__moving_down = True
+        self.max_speed = max_speed
+        self.initial = position
+        self.target = target
+        self.middle = [(self.initial[0] + self.target[0]) // 2, (self.initial[1] + self.target[1]) // 2]
+        #print("middle:", self.middle[0])
+
+    def movement(self):
+        moved = [self.x - self.initial[0] + 1, self.y - self.initial[1] + 1]
+        moved_middle = [self.middle[0] - self.initial[0], self.middle[1] - self.initial[1]]
+        moved_target = [self.target[0] - self.initial[0], self.target[1] - self.initial[1]]
+
+        if self.target[0] != self.initial[0]:
+            #print('move x')
+            x_progress = moved[0] / moved_target[0]
+
+            if self.x < self.middle[0]:
+                speed_x = (moved[0] / moved_middle[0]) * self.max_speed + 1
+            else:
+                speed_x = (1 - ((moved[0] - moved_middle[0]) / (moved_target[0] - moved_middle[0]))) * self.max_speed + 1
+            speed_x = min(round(speed_x), self.max_speed)
+
+            if self.__moving_right:
+                self.x += speed_x
+                self.velocity[0] = speed_x
+            else:
+                self.x -= speed_x
+                self.velocity[0] = -speed_x
+
+            if self.x > self.target[0]:
+                self.__moving_right = False
+            elif self.x < self.initial[0]:
+                self.__moving_right = True
+
+            last_y = self.y
+            self.y = self.initial[1] + (moved_target[1] * x_progress)
+            self.velocity[1] = self.y - last_y
+
+        elif self.target[1] != self.initial[1]:
+            if self.y < self.middle[1]:
+                speed_y = (moved[1] / moved_middle[1]) * self.max_speed + 1
+            else:
+                speed_y = (1 - ((moved[1] - moved_middle[1]) / (moved_target[1] - moved_middle[1]))) * self.max_speed + 1
+            speed_y = min(round(speed_y), self.max_speed)
+
+            if self.__moving_down:
+                self.y += speed_y
+                self.velocity[1] = speed_y
+            else:
+                self.y -= speed_y
+                self.velocity[1] = -speed_y
+
+            if self.y > self.target[1]:
+                self.__moving_down = False
+            elif self.y < self.initial[1]:
+                self.__moving_down = True
+
+class Header:
+    def __init__(self):
+        # load the images
+        self.health_front = pygame.image.load("images/level_1/HealthBarFront.png").convert_alpha()
+        self.health_back = pygame.image.load("images/level_1/HealthBarBack.png").convert_alpha()
+        self.header = pygame.image.load("images/level_1/header.png").convert_alpha()
+        self.coin_back = pygame.image.load("images/level_1/CoinBarBack.png").convert_alpha()
+        self.zero = pygame.image.load("images/level_1/zero.png").convert_alpha()
+        self.one = pygame.image.load("images/level_1/one.png").convert_alpha()
+        self.two = pygame.image.load("images/level_1/two.png").convert_alpha()
+        self.three = pygame.image.load("images/level_1/three.png").convert_alpha()
+        self.player_1_txt = pygame.image.load("images/level_1/player_one_txt.png").convert_alpha()
+        self.player_2_txt = pygame.image.load("images/level_1/player_two_txt.png").convert_alpha()
+        self.momo = pygame.image.load("images/MomotaroSprites/momotaroidle.png").convert_alpha()
+        self.bird = pygame.image.load("images/player2/flying_left.png").convert_alpha()
+
+        # scale images
+        self.health_front = pygame.transform.scale(self.health_front, (225, 30))
+        self.health_back = pygame.transform.scale(self.health_back, (300, 65))
+        self.header = pygame.transform.scale(self.header, (2200, 100))
+        self.coin_back = pygame.transform.scale(self.coin_back, (140, 65))
+        self.zero = pygame.transform.scale(self.zero, (125, 65))
+        self.one = pygame.transform.scale(self.one, (125, 65))
+        self.two = pygame.transform.scale(self.two, (125, 65))
+        self.three = pygame.transform.scale(self.three, (125, 65))
+        self.player_1_txt = pygame.transform.scale(self.player_1_txt, (200, 40))
+        self.player_2_txt = pygame.transform.scale(self.player_2_txt, (200, 40))
+        # self.player_2_txt = pygame.transform.scale(self.player_2_txt, (145, 50))
+        self.momo = pygame.transform.scale(self.momo, (50, 80))
+        self.bird = pygame.transform.scale(self.bird, (50, 80))
+
+    def draw_header(self, surface, momo_health, pet_health, coins):
+        # draw images to the screen
+        surface.blit(self.header, (-200, 0))
+        surface.blit(self.player_1_txt, (210, 30))
+        surface.blit(self.momo, (435, 10))
+
+        surface.blit(self.health_back, (540, 15))
+        health_len = 225 * (momo_health / 100)
+        self.health_front = pygame.transform.scale(self.health_front, (health_len, 30))
+        surface.blit(self.health_front, (598.5, 32))
+
+        surface.blit(self.coin_back, (890, 15))
+
+        if coins == 0:
+            surface.blit(self.zero, (900, 15))
+        elif coins == 1:
+            surface.blit(self.one, (900, 15))
+        elif coins == 2:
+            surface.blit(self.two, (900, 15))
+        else:
+            surface.blit(self.three, (900, 15))
+
+        surface.blit(self.player_2_txt, (1080, 30))
+        surface.blit(self.bird, (1305, 10))
+
+        surface.blit(self.health_back, (1410, 15))
+        health_len = 225 * (pet_health / 50)
+        self.health_front = pygame.transform.scale(self.health_front, (health_len, 30))
+        surface.blit(self.health_front, (1468.5, 32))
