9b436e54900b122bd336db45a8c8ff5db06e5fc2
Shawn Rhoads <76660850+shawn-rhoads@users.noreply.github.com>
2023-07-13 15:16:34 +0900

    when pet is out of view, lose the game

--- drivers/game_manager.py
+++ drivers/game_manager.py
@@ -9,6 +9,8 @@ import sys
 Purpose: The GameManager object contains level and player information and regularly updates and polls player 
             interactions with the environment provided by the level.
 '''
+
+
 # "Persistent State" Game Manager:
 class GameManager:
     def __init__(self, my_toolbox, level):
@@ -33,15 +35,16 @@ class GameManager:
 
         # Loading background image
         self.mountain_background = pygame.transform.scale(
-        #    pygame.image.load("images/backgrounds/mountains/parallax-mountain-bg-reduced.png").convert_alpha(), (1920, 1080))
+            #    pygame.image.load("images/backgrounds/mountains/parallax-mountain-bg-reduced.png").convert_alpha(), (1920, 1080))
             pygame.image.load("images/backgrounds/level_1_bkgnd.png").convert_alpha(), (1920, 915))
-        #self.far_mountains = pygame.image.load("images/backgrounds/mountains/parallax-mountain-mountains-reduced.png").convert_alpha()
+        # self.far_mountains = pygame.image.load("images/backgrounds/mountains/parallax-mountain-mountains-reduced.png").convert_alpha()
 
         self.controls = pygame.image.load("images/game_ui/controls.png").convert_alpha()
 
     '''
     Purpose: While the GameManager object is running, the main gameplay loop for the corresponding level occurs
     '''
+
     def run(self):
         # run event handling for the level until lvl_complete == True or broken out of
         while not self.level_complete:
@@ -57,7 +60,7 @@ class GameManager:
                             self.my_toolbox.adjusted_mouse_pos(event.pos)):  # if clicked pause button
                         return_st = pause_screen_scene.run(self.my_toolbox, self.level_name)
                         if return_st == "level_selector" or return_st == self.level_name:  # break out of running level
-                            #print('restarting')
+                            # print('restarting')
                             return return_st
 
                 # if key pressing, can either be pausing or ending the game
@@ -68,14 +71,14 @@ class GameManager:
 
                         # Poll pause scene next scene
                         if return_st == "level_selector" or return_st == self.level_name:  # break out of running level
-                            #print('restarting')
+                            # print('restarting')
                             return return_st
                     # up button pressed (W) at the tori gate, ending the level
                     if self.level.interactible_list["torigate"][0].is_pushed() and event.key == pygame.K_w:
                         win_return = win_screen_scene.run(self.my_toolbox, self.level_name, self.coins_collected)
                         self.update_save_file(self.level_name, self.coins_collected)
                         # Poll the win game scene next scene
-                        if win_return == "level_selector" or win_return == self.level_name or win_return ==  "quit":
+                        if win_return == "level_selector" or win_return == self.level_name or win_return == "quit":
                             return win_return
 
             # Checking for if the game is over/failed (Momo dead or out of bounds)
@@ -87,7 +90,7 @@ class GameManager:
                     return lose_rt
 
             # If momotaro is pushed below a block, he dies
-            
+
             if self.momotaro.standing_on:
                 if self.momotaro.position[
                     1] + self.momotaro.get_rect().height // 2 > self.momotaro.standing_on.get_rect().top:
@@ -96,16 +99,18 @@ class GameManager:
                         return lose_rt
 
             self.tick_physics()
-            self.draw()
+            val = self.draw()
+            if val is not None:
+                return val
 
             self.my_toolbox.clock.tick(60)
-
 
     '''
     Purpose: Main physics checker and updater for gameplay cycle. Updates character and enemy collision, movement, and
                 interaction with obstacles and with each other. Specific details on what's being updated belong within 
                 the character methods.
     '''
+
     def tick_physics(self):
         for moving_platform in self.level.moving_platform_list:
             moving_platform.movement()
@@ -125,15 +130,15 @@ class GameManager:
         self.pet.check_collision_interactible(self.level.interactible_list, self)
         self.pet.check_damage(self.level.demon_list)
 
-        #print(self.momotaro.position)
+        # print(self.momotaro.position)
         for demon in self.level.demon_list:
             demon.update_movement(self.momotaro, self.pet)
             demon.check_collisions(self.level.collidable_list)
 
-
     '''
     Purpose: Calls the draw function of each object and draws backgrounds
     '''
+
     def draw(self):
         view_surface = pygame.surface.Surface((1920, 1080))
 
@@ -157,9 +162,9 @@ class GameManager:
                 self.image.blit(self.controls, (170, 200))
 
                 # Far Mountains
-                #self.image.blit(self.far_mountains, (-544 + positional, 850))
-                #self.image.blit(self.far_mountains, (positional, 850))
-                #self.image.blit(self.far_mountains, (544 + positional, 850))
+                # self.image.blit(self.far_mountains, (-544 + positional, 850))
+                # self.image.blit(self.far_mountains, (positional, 850))
+                # self.image.blit(self.far_mountains, (544 + positional, 850))
 
         # Draw platforms
         for platform in self.level.platform_list:
@@ -186,7 +191,7 @@ class GameManager:
                     for obstacle in self.level.interactible_list[interactible_key]:
                         obstacle.draw(self.image)
                 case "coin":
-                    #self.level.coins_collected = 0
+                    # self.level.coins_collected = 0
                     for coin in self.level.interactible_list[interactible_key]:
                         if not coin.collected:
                             coin.draw(self.image)
@@ -196,22 +201,32 @@ class GameManager:
         self.pet.draw(self.image)
 
         if self.momotaro.get_rect().centerx <= 960:
-            view_surface.blit(self.image, (0, 0))
+            special_x = 0
         elif self.momotaro.get_rect().centerx >= self.level.width - 960:
-            view_surface.blit(self.image, (-(self.level.width - 1920), 0))
+            special_x = -(self.level.width - 1920)
         else:
-            view_surface.blit(self.image, ((-self.momotaro.get_rect().centerx) + (1920 / 2), 0))
+            special_x = (-self.momotaro.get_rect().centerx) + (1920 / 2)
+
+        view_surface.blit(self.image, (special_x, 0))
+
+        if self.pet.get_rect().centerx < -special_x or self.pet.get_rect().centerx > -special_x + 1920:
+            lose_rt = lose_screen_scene.run(self.my_toolbox, self.level_name)
+
+            # Poll next scene from lose screen
+            if lose_rt == "level_selector" or lose_rt == self.level_name or lose_rt == "quit":
+                return lose_rt
 
         # Draw Header
         self.level.header.draw_header(view_surface, self.momotaro.health, self.pet.health, self.coins_collected)
 
-        #self.pause_btn.draw(view_surface, (80, 65))
+        # self.pause_btn.draw(view_surface, (80, 65))
         self.my_toolbox.draw_to_screen(view_surface)
         pygame.display.update()
 
     """
     Purpose: Saves the number of coins collected upon level clear to update on the select screen
     """
+
     def update_save_file(self, level_name, coins_collected):
         # get current info from the save file
         with open("save_data/game_data", 'r') as file:
