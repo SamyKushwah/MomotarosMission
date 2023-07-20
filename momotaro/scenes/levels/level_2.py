523092ca9aca21942971cb82429bf5fdc26ed116
Kevin C <k.chen1@ufl.edu>
2023-07-20 10:57:24 +0900

    small level 2 adjustments

--- momotaro/scenes/levels/level_2.py
+++ momotaro/scenes/levels/level_2.py
@@ -27,8 +27,8 @@ def create_level(my_toolbox):
     level2.add_platform((1280, 1080 - 70), (750, 70), platform_type="water")   # water section 2 (bottom)
     level2.add_platform((4600, 1080 - 70), (430, 70), platform_type="water")   # water section 3 (bottom)
 
-    level2.add_platform((350, 340), (650, 70), platform_type="water")          # water section 1 (top)
-    level2.add_platform((1280, 410), (750, 70), platform_type="water")         # water section 2 (top)
+    level2.add_platform((350, 340 + 20), (650, 70), platform_type="water")          # water section 1 (top)
+    level2.add_platform((1280, 410 + 20), (750, 70), platform_type="water")         # water section 2 (top)
 
     # add moving platforms throughout
     level2.add_moving_platform((400, 940), (300, 50), 4, (600, 940), facing_direction="up")    # first water section
@@ -43,10 +43,11 @@ def create_level(my_toolbox):
     level2.add_platform((70, 340), (280, 140), facing_direction="up")       # first section (top)
     level2.add_platform((350, 410), (370, 70),  facing_direction=None)
     level2.add_platform((720, 410), (280, 210),  facing_direction=None)
+    level2.add_platform((480, 590), (240, 30), facing_direction="up")
     level2.add_platform((1000, 340), (280, 420), facing_direction="up")
     level2.add_platform((1280, 480), (750, 70), facing_direction=None)
     level2.add_platform((2030, 410), (250, 140), facing_direction="up")
-    level2.add_platform((1430, 310), (450, 50), facing_direction="up")      # above second water section (top)
+    level2.add_platform((1400, 330), (510, 50), facing_direction="up")      # above second water section (top)
 
     level2.add_platform((2450, 545), (200, 50), facing_direction="up")      # solo floating platform
 
@@ -57,12 +58,12 @@ def create_level(my_toolbox):
     level2.add_platform((4715, 900), (200, 50), facing_direction="up")      # low platform with final coin
 
     # add button/gate obstacles
-    level2.add_obstacle(600, 480, "button", fence_initial=(1075, 140),  fence_final=(1075, -60), fence_dimensions=(150, 400))     # first top gate
+    level2.add_obstacle(600, 580, "button", fence_initial=(1075, 140),  fence_final=(1075, -60), fence_dimensions=(150, 400))     # first top gate
     level2.add_obstacle(1200, 330, "button", fence_initial=(2105, 210), fence_final=(2105, -60), fence_dimensions=(150, 400))     # second top gate
-    level2.add_obstacle(4925, 500, "button", fence_initial=(3375, 200), fence_final=(3375, -60), fence_dimensions=(150, 400))     # third top gate
-    level2.add_obstacle(3550, 1000, "button", fence_initial=(4675, 300), fence_final=(4675, -60), fence_dimensions=(150, 400))    # fourth top gate
+    level2.add_obstacle(4900, 500, "button", fence_initial=(3375, 200), fence_final=(3375, -60), fence_dimensions=(150, 400))     # third top gate
+    level2.add_obstacle(3650, 1000, "button", fence_initial=(4675, 300), fence_final=(4675, -60), fence_dimensions=(150, 400))    # fourth top gate
     level2.add_obstacle(1210, 1000, "button", fence_initial=(2105, 778), fence_final=(2105, 320), fence_dimensions=(150, 465))    # first bottom gate
-    level2.add_obstacle(3650, 1000, "button", fence_initial=(3925, 778), fence_final=(3925, 330), fence_dimensions=(150, 465))    # second bottom gate
+    level2.add_obstacle(3750, 1000, "button", fence_initial=(3925, 778), fence_final=(3925, 330), fence_dimensions=(150, 465))    # second bottom gate
 
     # add demons
     level2.add_demon([1655, 200], (300, 100))
